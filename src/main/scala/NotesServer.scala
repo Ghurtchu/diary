f7015adc712ceb7fbf8d78b70e27ef41fa1dc3f9
nghurtch <nghurtch@broadinstitute.com>
2022-08-14 13:27:54 +0400

    initiate user sign-up functionality

--- src/main/scala/NotesServer.scala
+++ src/main/scala/NotesServer.scala
@@ -5,12 +5,30 @@ import zio.*
 import zhttp.*
 import zhttp.http.*
 import service.Server
-import zio.json._
-
+import zio.json.*
 
 object NotesServer extends ZIOAppDefault {
 
   val httpApp: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
+    case req @ Method.POST -> !! / "api" / "user" / "sign-up" =>  {
+      for {
+        body      <- req.bodyAsString
+        bodyAsMap <- ZIO.succeed(body.fromJson[Map[String, String]])
+        statusOrError  <- ZIO.succeed {
+          bodyAsMap.map { body =>
+            val hasCorrectKeys = body.isDefinedAt("email") && body.isDefinedAt("password")
+
+            hasCorrectKeys
+          }
+        }
+        response <- ZIO.succeed {
+          statusOrError.fold(
+            err => Response text err,
+            status => if status then Response.text("correct data") else Response.text("incorrect data")
+          )
+        }
+      } yield response
+    }
     case Method.GET        -> !! / "api" / "notes"            => GetAllNotesRoute().handle
     case req @ Method.POST -> !! / "api" / "notes"            => req.bodyAsString.flatMap(CreateNoteRoute(_).handle)
     case req @ Method.POST -> !! / "api" / "notes" / "search" => SearchNoteRoute().handle(req.url.queryParams("title").head)

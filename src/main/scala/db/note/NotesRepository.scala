eb5569796d59b7d61b101bac33dad4594322d308
nghurtch <nghurtch@broadinstitute.com>
2022-09-01 21:40:28 +0400

    implement the rest of the methods in NotesRepository

--- src/main/scala/db/note/NotesRepository.scala
+++ src/main/scala/db/note/NotesRepository.scala
@@ -1,5 +1,6 @@
 package db.note
 
+import org.mongodb.scala.Document
 import db.CRUD.{CreationStatus, DeletionStatus, UpdateStatus}
 import db.InMemoryDB
 import db.mongo.{MongoDatabaseBuilder, MongoDatabaseProvider}
@@ -22,51 +23,51 @@ final case class NotesRepository() extends NoteCRUD {
   private final lazy val mongo: UIO[MongoDatabase] = MongoDatabaseProvider.get
 
   override def getById(id: Long): Task[Option[Note]] = for {
-    db          <- mongo
-    queryResult <- ZIO.fromFuture { implicit ec =>
+    db        <- mongo
+    document  <- ZIO.fromFuture { implicit ec =>
       db.getCollection("notes")
-        .find(equal("id", s"Long($id)"))
+        .find(equal("id", id))
         .first()
         .toFuture()
     }
-    maybeNote   <- ZIO.succeed {
-      Option(queryResult).fold(None){ doc =>
-        Some(
-          Note(
-            id        = id,
-            title     = doc("title").asString.getValue,
-            body      = doc("body").asString.getValue,
-            createdAt = doc("createdAt").asString.getValue,
-            userId    = doc("userId").asInt64.getValue
-         )
-        )
-      }
-    }
+    maybeNote <- ZIO.succeed(parseDocumentToNote(document))
   } yield maybeNote
 
   override def getAll: Task[List[Note]] = for {
-    db          <- mongo
-    queryResult <- ZIO.fromFuture { implicit ec =>
+    db        <- mongo
+    documents <- ZIO.fromFuture { implicit ec =>
       db.getCollection("notes")
         .find()
         .toFuture()
     }
-    notes       <- ZIO.succeed {
-      queryResult.map { doc =>
-        Note(
-          id        = doc("id").asInt64.getValue,
-          title     = doc("title").asString.getValue,
-          body      = doc("body").asString.getValue,
-          createdAt = doc("createdAt").asString.getValue,
-          userId    = doc("userId").asInt64.getValue
-        )
-      }.toList
-    }
+    notes     <- ZIO.succeed(parseDocumentsToNoteList(documents))
   } yield notes
 
-  override def update(id: Long, newNote: Note): Task[UpdateStatus] = ZIO.attempt(Left("TODO"))
+  override def update(id: Long, newNote: Note): Task[UpdateStatus] = for {
+    db          <- mongo
+    queryResult <- ZIO.fromFuture { implicit ec =>
+      db.getCollection("notes")
+        .updateOne(equal("id", id), Document(newNote.toJson))
+        .toFuture()
+    }
+    updateStatus <- ZIO.succeed {
+      if queryResult.wasAcknowledged then Right(s"Note with id '$id' has been updated successfully")
+      else Left(s"Note with id '$id' has not been updated'")
+    }
+  } yield updateStatus
 
-  override def delete(noteId: Long): Task[DeletionStatus] = ZIO.attempt(Left("TODO"))
+  override def delete(noteId: Long): Task[DeletionStatus] = for {
+    db             <- mongo
+    queryResult    <- ZIO.fromFuture { implicit ec =>
+      db.getCollection("notes")
+        .deleteOne(equal("id", noteId))
+        .toFuture()
+    }
+    deletionStatus <- ZIO.succeed{
+      if queryResult.wasAcknowledged then Right(s"Note with id '$noteId' has been deleted")
+      else Left(s"Note with id '$noteId' has not been deleted")
+    }
+  } yield deletionStatus
 
   override def add(note: Note): Task[CreationStatus] = for {
     noteWithId     <- ZIO.succeed(note.copy(id = Some(scala.util.Random.nextLong(Long.MaxValue))))
@@ -80,54 +81,43 @@ final case class NotesRepository() extends NoteCRUD {
   } yield creationStatus
 
   override def getNotesByUserId(userId: Long): Task[List[Note]] = for {
-    db          <- mongo
-    queryResult <- ZIO.fromFuture { implicit ec =>
+    db        <- mongo
+    documents <- ZIO.fromFuture { implicit ec =>
       db.getCollection("notes")
         .find(equal("userId", userId))
         .toFuture()
     }
-    notes       <- ZIO.succeed {
-      queryResult.map { doc =>
+    notes     <- ZIO.succeed(parseDocumentsToNoteList(documents))
+  } yield notes
+
+  override def getNoteByIdAndUserId(id: Long, userId: Long): Task[Option[Note]] = for {
+    db       <- mongo
+    document <- ZIO.fromFuture { implicit ec =>
+      db.getCollection("notes")
+        .find(and(equal("id", id), equal("userId", userId)))
+        .first()
+        .toFuture()
+    }
+    note     <- ZIO.attempt(parseDocumentToNote(document))
+  } yield note
+
+  override def deleteNoteByIdAndUserId(noteId: Long, userId: Long): Task[DeletionStatus] = ZIO.attempt(Left("TODO"))
+
+  private def parseDocumentToNote(document: Document): Option[Note] = {
+    Option(document).fold(None) { doc =>
+      Some(
         Note(
           id        = doc("id").asInt64.getValue,
           title     = doc("title").asString.getValue,
           body      = doc("body").asString.getValue,
           createdAt = doc("createdAt").asString.getValue,
-          userId    = userId
+          userId    = doc("userId").asInt64.getValue
         )
-      }.toList
+      )
     }
-  } yield notes
+  }
 
-  override def getNoteByIdAndUserId(id: Long, userId: Long): Task[Option[Note]] = for {
-    db          <- mongo
-    queryResult <- ZIO.fromFuture { implicit ec =>
-      db.getCollection("notes")
-        .find(
-          and(
-            equal("id", id),
-            equal("userId", userId)
-          )
-        )
-        .first()
-        .toFuture()
-    }
-    notes <- ZIO.succeed {
-      Option(queryResult).fold(None) { doc =>
-        Some(
-          Note(
-          id        = doc("id").asInt64.getValue,
-          title     = doc("title").asString.getValue,
-          body      = doc("body").asString.getValue,
-          createdAt = doc("createdAt").asString.getValue,
-          userId    = userId
-         )
-        )
-      }
-    }
-  } yield notes
-
-  override def deleteNoteByIdAndUserId(noteId: Long, userId: Long): Task[DeletionStatus] = ZIO.attempt(Left("TODO"))
+  private def parseDocumentsToNoteList(documents: Seq[Document]) = documents.map(parseDocumentToNote).toList.flatten
 
 }
 

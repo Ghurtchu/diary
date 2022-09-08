329df1a76cbbc4292e80281029e1b65980924ee6
nghurtch <nghurtch@broadinstitute.com>
2022-09-08 17:14:30 +0400

    use Scala 3 enums instead of Scala 2 sealed traits

---
+++ src/main/scala/util/sort/SortOrder.scala
@@ -0,0 +1,12 @@
+package util.sort
+
+enum SortOrder:
+
+  self =>
+
+  case Ascending
+  case Descending
+
+  def fold[A](ifAscending: => A)(ifDescending: => A): A = self match
+    case SortOrder.Ascending  => ifAscending
+    case SortOrder.Descending => ifDescending
--- src/main/scala/util/sort/SortService.scala
+++ src/main/scala/util/sort/SortService.scala
@@ -5,13 +5,3 @@ import zio.Task
 trait SortService[A]:
   def sort(sortOrder: SortOrder, userId: Long): Task[List[A]]
 
-sealed trait SortOrder { self =>
-  def fold[A](ifAscending: => A)(ifDescending: => A): A = self match
-    case SortOrder.Ascending  => ifAscending
-    case SortOrder.Descending => ifDescending
-}
-
-object SortOrder:
-  case object Ascending  extends SortOrder
-  case object Descending extends SortOrder
-

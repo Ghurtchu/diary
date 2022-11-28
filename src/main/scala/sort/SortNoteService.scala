ce4e2cdacf24d25f50efd527c435111d0283a6b4
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2022-11-28 12:40:46 +0400

    refactor sorting mechanism

--- src/main/scala/sort/SortNoteService.scala
+++ src/main/scala/sort/SortNoteService.scala
@@ -3,18 +3,23 @@ package scala.sort
 import db.*
 import db.note.NotesRepository
 import model.Note
-import zio._
+import zio.*
+
+import scala.sort.SortNoteService.{ascending, descending}
 
 final case class SortNoteService(notesRepository: NotesRepository) extends SortService[Note]:
 
   def sort(sortOrder: SortOrder, userId: Long): Task[List[Note]] = 
     for 
       notes  <- notesRepository getNotesByUserId userId
-      sorted <- ZIO.succeed(sortOrder.fold(notes.sortWith(_.title < _.title))(notes.sortWith(_.title > _.title)))
+      sorted <- ZIO.succeed(sortOrder.fold(notes.sortWith(ascending))(notes.sortWith(descending)))
     yield sorted
 
 
 object SortNoteService:
-  
+
+    val ascending: (Note, Note) => Boolean = _.title < _.title
+    val descending: (Note, Note) => Boolean = _.title > _.title
+
     lazy val layer: URLayer[NotesRepository, SortNoteService] = ZLayer.fromFunction(SortNoteService.apply)
 

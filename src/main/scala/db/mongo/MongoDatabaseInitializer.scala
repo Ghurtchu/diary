5de3bec33bd2fc955d6f21bafebf0b3c3d21e258
Anzori Ghurtchumelia <anzorighurtchumelia@Anzoris-MacBook-Pro.local>
2022-11-28 11:28:27 +0400

    fix problems in database initialization

--- src/main/scala/db/mongo/MongoDatabaseInitializer.scala
+++ src/main/scala/db/mongo/MongoDatabaseInitializer.scala
@@ -1,16 +1,32 @@
 package db.mongo
 
+import com.mongodb.event.CommandListener
 import zio.*
 import org.mongodb.scala.*
+import org.mongodb.scala.connection.ConnectionPoolSettings
+
+import java.net.SocketTimeoutException
+import java.util.concurrent.TimeUnit
 
 final case class MongoDatabaseInitializer(dataSource: DataSource) extends DatabaseInitializer:
 
+  private final val mongoServerSettings = ZIO.succeed {
+    MongoClientSettings.builder()
+      .applyToClusterSettings(_.serverSelectionTimeout(5, TimeUnit.SECONDS))
+      .build()
+  }
+
   override def initialize(dbConfig: DBConfig): UIO[Unit] =
     (for
-      _      <- Console.printLine(s"Attempting to establish the connection with MongoDB on port: ${dbConfig.port} with db ${dbConfig.name}")
-      client <- ZIO.attempt(MongoClient(dbConfig.port))
-      db     <- ZIO.attempt(client.getDatabase(dbConfig.name)) <* Console.printLine("Established connection with database successfully")
-      _      <- dataSource.setCtx(DatabaseContext(db))
+      _        <- Console.printLine(s"Attempting to establish the connection with MongoDB on port: ${dbConfig.port} with db ${dbConfig.name}")
+      settings <- mongoServerSettings
+      client   <- ZIO.attempt(MongoClient(settings))
+      db       <- ZIO.attempt(client.getDatabase(dbConfig.name))
+      _        <- ZIO.fromFuture(implicit ec => db.listCollectionNames.toFuture).catchSome {
+        case mte: MongoTimeoutException => ZIO.fail(RuntimeException(s"Connecting to MongoDB failed, reason: ${mte.getMessage}"))
+        case _ => ZIO.fail(RuntimeException("Connecting to MongoDB failed, reason unknown"))
+      }
+      _        <- dataSource.setCtx(DatabaseContext(db))
     yield ()).orDie
 
 
